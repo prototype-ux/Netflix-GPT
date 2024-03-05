7090332deb81e2c2a3edc438c9d59f3cb068c547
prototype-ux <mansi.kesarwani987@gmail.com>
2024-03-05 20:21:17 +0530

    signIn/signUp form on Login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+    const [isSignIn,setisSignIn]=useState(true)
+    const toggleform=()=>{
+            setisSignIn(!isSignIn)
+    }
   return (
     <div>
       <Header/>
@@ -11,11 +15,12 @@ const Login = () => {
       
       </div>
       <form className='bg-black rounded-md bg-opacity-80 absolute mx-auto w-3/12 my-36 right-0 left-0 p-12 text-white text-center'> 
-      <h1 className='text-3xl font-bold'>Sign In</h1>
-        <input type='text' placeholder='Email Address' className='p-3 my-3 mt-6 w-full bg-gray-500'/>
-        <input type="password" placeholder='Password' className='p-3 my-3 w-full bg-gray-500' /> 
-        <button className='p-2 my-3  bg-red-700 w-full rounded-md '>Submit</button>
-        <p>New to Netflix? <span className='font-semibold text-red-700'> Sign Up</span> now!!</p>
+      <h1 className='text-3xl font-bold mb-6'>{isSignIn? "Sign In":"Sign Up"}</h1>
+      {!isSignIn && <input type='text' placeholder='Name' className='p-2 my-3 w-full rounded-md  bg-gray-500'/>}
+        <input type='text' placeholder='Email Address' className='p-2 my-3 w-full rounded-md  bg-gray-500'/>
+        <input type="password" placeholder='Password' className='p-2 my-3 w-full rounded-md  bg-gray-500' /> 
+        <button className='p-2 my-3  bg-red-700 w-full rounded-md '>{isSignIn? "Sign In":"Sign Up"}</button>
+        <p>{isSignIn? "New to Netflix":"Already a User?"} <span className='font-semibold text-red-700 cursor-pointer' onClick={toggleform}>{isSignIn? "Sign Up":"Sign In"}</span> now!!</p>
       </form>
     </div>
   )
